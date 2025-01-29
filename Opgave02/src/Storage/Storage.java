d8e6e0e6d72110267bae9c8cd57c45bcea7dc5cf
benn-christensen <bnch@eaaa.dk>
2025-01-29 11:58:05 +0100

    contains check in add methods

--- Opgave02/src/Storage/Storage.java
+++ Opgave02/src/Storage/Storage.java
@@ -7,19 +7,25 @@ import Models.TVSerie;
 import java.util.ArrayList;
 
 public class Storage {
-    private ArrayList<Movie> movies = new ArrayList<Movie>();
+    private ArrayList<Movie> movies = new ArrayList<>();
     private ArrayList<TVSerie> series = new ArrayList<>();
-    private ArrayList<Actor> actors = new ArrayList<Actor>();
+    private ArrayList<Actor> actors = new ArrayList<>();
 
     public void addMovie(Movie movie) {
-        movies.add(movie);
+        if (!movies.contains(movie)) {
+            movies.add(movie);
+        }
     }
 
     public void addActor(Actor actor) {
-        actors.add(actor);
+        if (!actors.contains(actor)) {
+            actors.add(actor);
+        }
     }
 
     public void addTVSerie(TVSerie serie) {
-        series.add(serie);
+        if (!series.contains(serie)) {
+            series.add(serie);
+        }
     }
 }
